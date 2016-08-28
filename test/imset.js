261e16ecaa99825804118d65790d98371ba45c38
Frankie Bagnardi <f.bagnardi@gmail.com>
2016-08-28 11:36:46 -0700

    adds immutability test

--- test/imset.js
+++ test/imset.js
@@ -1,20 +1,33 @@
 import test from "tape"
 import imset from "../src"
+import assert from 'assert';
+import {cloneDeep, isEqual} from 'lodash';
+
+function prepareImmutableCheck(obj) {
+  var cloned = cloneDeep(obj);
+  return () => {
+    assert.ok(isEqual(obj, cloned), 'there were no mutations');
+  };
+}
 
 test("imset", (t) => {
   t.test('simple set', (t) => {
     var a = {x: 1};
+    var ic = prepareImmutableCheck(a);
     var b = imset`${a}.x = 2`;
     t.equal(a.x, 1);
     t.deepEqual(b, {x: 2});
+    ic();
     t.end();
   });
 
   t.test('simple method', (t) => {
     var a = {x: {y: [1]}};
+    var ic = prepareImmutableCheck(a);
     var b = imset`${a}.x.y.push(2)`;
     t.deepEqual(a.x.y, [1]);
     t.deepEqual(b, {x: {y: [1, 2]}});
+    ic();
     t.end();
   });
 
